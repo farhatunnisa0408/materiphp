12cd64e1d065319d2fc88390bb8f1db182e8552f
farhatunnisa0408 <123419469+farhatunnisa0408@users.noreply.github.com>
2023-05-15 10:33:15 +0700

    Create latihan1.php

---
+++ latihan1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Percobaan PHP</title>
+</head>
+<body>
+   <?php echo "<p>HELLO WORD</p>";?>
+   <!-- echo untuk mencetak hasil -->
+
+</body>
+</html>
